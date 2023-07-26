25360a3a14f030fc646c1d6e31f4de0941e6c126
James Pettigrew <git@jamespettigrew.com>
2023-07-26 23:30:06 +1000

    feat: add some egui widgets

--- src/main.rs
+++ src/main.rs
@@ -7,42 +7,116 @@ mod util;
 use crate::cpu::{Cpu, RegisterWide};
 use crate::memory::Memory;
 
+use eframe::egui;
+
 use std::fs;
 use std::io::Read;
 use std::path::Path;
 
 type ROM = Vec<u8>;
 
-fn main() {
-    let bootstrap_rom = open_rom(Path::new("./roms/bootstrap.gb"));
-    disassemble(&bootstrap_rom);
+enum State {
+    Paused,
+    Running,
+}
 
-    let cpu = &mut Cpu::init();
-    let memory = &mut Memory::init();
-    map_rom_into_memory(&bootstrap_rom, memory);
+struct Jameboy {
+    cpu: Cpu,
+    memory: Memory,
+    prefixed: bool,
+    state: State,
+}
 
-    let mut executing = true;
-    let mut prefixed = false;
-    while executing {
-        println!("{:?}", cpu);
-        let pc = cpu.read_register_wide(RegisterWide::PC);
-        let byte = memory.read(pc);
-        let opcode = if prefixed {
-            prefixed = false;
+impl Jameboy {
+    fn init() -> Self {
+        Self {
+            cpu: Cpu::init(),
+            memory: Memory::init(),
+            prefixed: false,
+            state: State::Paused,
+        }
+    }
+
+    fn step(&mut self) {
+        let pc = self.cpu.read_register_wide(RegisterWide::PC);
+        let byte = self.memory.read(pc);
+        let opcode = if self.prefixed {
+            self.prefixed = false;
             opcode::decode_prefixed(byte)
         } else {
             opcode::decode(byte)
         };
 
         if opcode.is_none() {
-            executing = false;
-            continue;
+            return;
         }
 
         let opcode = opcode.unwrap();
-        opcode.execute(cpu, memory);
-        cpu.write_register_wide(RegisterWide::PC, pc + opcode.size_bytes as u16);
+        opcode.execute(&mut self.cpu, &mut self.memory);
+        self.cpu
+            .write_register_wide(RegisterWide::PC, pc + opcode.size_bytes as u16);
     }
+}
+
+fn main() {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(640.0, 480.0)),
+        ..Default::default()
+    };
+
+    let bootstrap_rom = open_rom(Path::new("./roms/bootstrap.gb"));
+    let mut jameboy = Jameboy::init();
+    map_rom_into_memory(&bootstrap_rom, &mut jameboy.memory);
+
+    eframe::run_simple_native("jameboy", options, move |ctx, _frame| {
+        render(ctx, &mut jameboy);
+        if let State::Running = jameboy.state {
+            jameboy.step();
+        }
+    });
+}
+
+fn render(ctx: &egui::Context, jameboy: &mut Jameboy) {
+    egui::CentralPanel::default().show(ctx, |ui| {
+        egui::Window::new("CPU").show(ctx, |ui| {
+            ui.heading("Registers");
+            ui.label(format!(
+                "AF: {:04x}",
+                jameboy.cpu.read_register_wide(RegisterWide::AF)
+            ));
+            ui.label(format!(
+                "BC: {:04x}",
+                jameboy.cpu.read_register_wide(RegisterWide::BC)
+            ));
+            ui.label(format!(
+                "DE: {:04x}",
+                jameboy.cpu.read_register_wide(RegisterWide::DE)
+            ));
+            ui.label(format!(
+                "HL: {:04x}",
+                jameboy.cpu.read_register_wide(RegisterWide::HL)
+            ));
+        });
+
+        egui::Window::new("Program").show(ctx, |ui| {
+            ui.horizontal(|ui| match jameboy.state {
+                State::Paused => {
+                    if ui.button("⏵").clicked() {
+                        jameboy.state = State::Running;
+                    }
+                    if ui.button("⏭").clicked() {
+                        jameboy.step();
+                    }
+                }
+                State::Running => {
+                    if ui.button("⏸").clicked() {
+                        jameboy.state = State::Paused;
+                    }
+                    ui.add_enabled(false, egui::Button::new("⏭"));
+                }
+            });
+        });
+    });
 }
 
 fn map_rom_into_memory(rom: &ROM, memory: &mut Memory) {
