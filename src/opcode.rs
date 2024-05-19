397141ae9a4ecc1884c29afb3f325c54a38024d1
James Pettigrew <git@jamespettigrew.com>
2024-05-19 18:43:52 +1000

    fix: ADC A, r8
    Need to compute carry/half-carry on addition of operands and carry
    individually, otherwise cary information can get lost.
    
    Additional info:
    https://stackoverflow.com/a/42097067

--- src/opcode.rs
+++ src/opcode.rs
@@ -3239,22 +3239,23 @@ fn add_r8(cpu: &mut Cpu, r: Register) {
 
 fn adc_r8(cpu: &mut Cpu, r: Register) {
     let a = cpu.read_register(Register::A);
-    let carry_bit = cpu.read_flags().carry as u8;
-    let b = cpu.read_register(r).wrapping_add(carry_bit);
-    let (mut result, mut overflowed) = a.overflowing_add(b);
+    let b = cpu.read_register(r);
+    let (mut result, mut carried) = a.overflowing_add(b);
+    let mut half_carried = half_carried_add8(a, b);
 
     if cpu.read_flags().carry {
+        half_carried |= half_carried_add8(result, 1);
         let (carry_result, carry_overflowed) = result.overflowing_add(1);
+        carried |= carry_overflowed;
         result = carry_result;
-        overflowed |= carry_overflowed;
     }
 
     cpu.write_register(Register::A, result);
     cpu.write_flags(WriteFlags {
         zero: Some(result == 0),
         subtract: Some(false),
-        half_carry: Some(half_carried_add8(a, b)),
-        carry: Some(overflowed),
+        half_carry: Some(half_carried),
+        carry: Some(carried),
     });
 }
 
