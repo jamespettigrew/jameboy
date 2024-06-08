a79faa848004cb4cd28f82e326145d00380e50dd
James Pettigrew <git@jamespettigrew.com>
2024-06-08 19:09:40 +1000

    refactor: clean up joypad logic

--- src/joypad.rs
+++ src/joypad.rs
@@ -2,56 +2,59 @@ use crate::memory::{Address, Memory};
 
 const ADDRESS_JOYP_REGISTER: u16 = 0xFF00;
 
+const KEY_RIGHT: egui::Key = egui::Key::D;
+const KEY_LEFT: egui::Key = egui::Key::A;
+const KEY_UP: egui::Key = egui::Key::W;
+const KEY_DOWN: egui::Key = egui::Key::S;
+const KEY_A: egui::Key = egui::Key::J;
+const KEY_B: egui::Key = egui::Key::K;
+const KEY_SELECT: egui::Key = egui::Key::Enter;
+const KEY_START: egui::Key = egui::Key::Escape;
+
 pub fn handle_input(ctx: &egui::Context, memory: &mut Memory) {
     let mut register = 0xFF;
     
-    // Right
-    if ctx.input(|i| i.key_pressed(egui::Key::D)) {
-        register &= 0b1111_1110;
-        register &= 0b1110_1111;
-    }
+    ctx.input(|i| {
+        if i.key_pressed(KEY_RIGHT) {
+            register &= 0b1111_1110;
+            register &= 0b1110_1111;
+        }
 
-    // Left
-    if ctx.input(|i| i.key_pressed(egui::Key::A)) {
-        register &= 0b1111_1101;
-        register &= 0b1110_1111;
-    }
+        if i.key_pressed(KEY_LEFT) {
+            register &= 0b1111_1101;
+            register &= 0b1110_1111;
+        }
 
-    // Up
-    if ctx.input(|i| i.key_pressed(egui::Key::W)) {
-        register &= 0b1111_1011;
-        register &= 0b1110_1111;
-    }
+        if i.key_pressed(KEY_UP) {
+            register &= 0b1111_1011;
+            register &= 0b1110_1111;
+        }
 
-    // Down
-    if ctx.input(|i| i.key_pressed(egui::Key::S)) {
-        register &= 0b1111_0111;
-        register &= 0b1110_1111;
-    }
+        if i.key_pressed(KEY_DOWN) {
+            register &= 0b1111_0111;
+            register &= 0b1110_1111;
+        }
 
-    // A
-    if ctx.input(|i| i.key_pressed(egui::Key::J)) {
-        register &= 0b1111_1110;
-        register &= 0b1110_1111;
-    }
+        if i.key_pressed(KEY_A) {
+            register &= 0b1111_1110;
+            register &= 0b1110_1111;
+        }
 
-    // B
-    if ctx.input(|i| i.key_pressed(egui::Key::K)) {
-        register &= 0b1111_1101;
-        register &= 0b1101_1111;
-    }
+        if i.key_pressed(KEY_B) {
+            register &= 0b1111_1101;
+            register &= 0b1101_1111;
+        }
 
-    // Select
-    if ctx.input(|i| i.key_pressed(egui::Key::Enter)) {
-        register &= 0b1111_1011;
-        register &= 0b1101_1111;
-    }
+        if i.key_pressed(KEY_SELECT) {
+            register &= 0b1111_1011;
+            register &= 0b1101_1111;
+        }
 
-    // Start
-    if ctx.input(|i| i.key_pressed(egui::Key::Escape)) {
-        register &= 0b1111_0111;
-        register &= 0b1101_1111;
-    }
+        if i.key_pressed(KEY_START) {
+            register &= 0b1111_0111;
+            register &= 0b1101_1111;
+        }
+    });
 
     memory.write(Address(ADDRESS_JOYP_REGISTER), register);
 }
